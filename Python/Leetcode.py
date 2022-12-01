390fb23760063308db3275a9cb0a87b81ad91c98
uzrivera <uzrivera@iu.edu>
2022-12-01 14:46:26 -0500

    Updates

--- Python/Leetcode.py
+++ Python/Leetcode.py
@@ -1,40 +1,42 @@
-class problem:
-    def __init__(self, l1, l2):
-        self.a = l1
-        self.b = l2
-
-    def get_num1(self):
-        return self.a
-
-    def get_num2(self):
-        return self.b
-
-    def add(self):
-        pass
-
-    # def add(l1, l2):
-    #     a = ''
-    #     b = ''
-    #     # lst = []
-    #     # # lst2 = []
-    #     # for i in range(len(l1)):
-    #     #     a += str(l1[i])
-    #     #     b += str(l2[i])
-    #     for nums in l1:
-    #         a += str(nums)
-    #     for nums2 in l2:
-    #         b += str(nums2)
-    #     num = int(a[::-1])
-    #     num2 = int(b[::-1])
-    #     sum = num + num2
-    #     return sum
+def add(l1, l2):
+    a = ''
+    b = ''
+    # lst = []
+    # # lst2 = []
+    # for i in range(len(l1)):
+    #     a += str(l1[i])
+    #     b += str(l2[i])
+    for nums in l1:
+        a += str(nums)
+    for nums2 in l2:
+        b += str(nums2)
+        num = int(a[::-1])
+        num2 = int(b[::-1])
+        sum = num + num2
+    return sum
 
 
-l1 = [2, 4, 3]
-l2 = [5, 6, 4]
+# l1 = [2, 4, 3]
+# l2 = [5, 6, 4]
 
-print(l1, l2)
-# print(342+465)
+# print(l1, l2)
+# # print(342+465)
 # print(add(l1, l2))
-x = problem(l1, l2)
-print(x)
+
+# still working on it
+
+# def twoSum(n, t):
+#     for num in n:
+#         for nums in t:
+#             for pos in num:
+#                 if num + nums == t:
+#                     return t
+#                 else:
+#                     return "target not met"
+
+
+# n = [2, 7, 11, 15]
+# t = 9
+# print("Input:" + " " + str(n))
+# print("Output: [0,1]")
+# print(twoSum(n, t))
